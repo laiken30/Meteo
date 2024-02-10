a6ec6efe55281206f40d6ceb81c4fde611861a27
Laiken Chetty <141495712+laiken30@users.noreply.github.com>
2024-02-10 20:51:30 +0200

    Updated API code

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "c10ca71f4cd918886fo5fbb38eta938d";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
